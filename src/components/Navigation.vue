99bc19d43eaa26df5a2cfeec1e7096990102191b
Andrey Kalashnik <kalashnik.a.p@gmail.com>
2020-03-10 21:02:45 +0100

    base structure setup. connect to firebase

---
+++ src/components/Navigation.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand bg-primary navbar-dark">
+      <div class="container-fluid">
+        <router-link class="navbar-brand" to="/">Meeting log</router-link>
+        <div class="navbar-nav ml-auto">
+          <router-link class="nav-item nav-link" to="/meetings">meetings</router-link>
+          <router-link class="nav-item nav-link" to="/login">login</router-link>
+          <router-link class="nav-item nav-link" to="/register">register</router-link>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
---
+++ src/views/Meetings.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Meetings page</h1>
+  </div>
+</template>
