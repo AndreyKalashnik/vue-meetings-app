350ef2ce757e56c636f05795f1e5519a0704340f
Andrey Kalashnik <kalashnik.a.p@gmail.com>
2020-03-12 08:51:49 +0100

    list of meetings. delete

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,27 @@
 <template>
   <div id="app">
     <Navigation v-bind:user="user" @logout="logout" />
-    <router-view class="container" v-bind:user="user" @addMeeting="addMeeting" />
+    <router-view
+      class="container"
+      v-bind:user="user"
+      v-bind:meetings="meetings"
+      @addMeeting="addMeeting"
+      @deleteMeeting="deleteMeeting"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import Firebase from "firebase";
 import Navigation from "@/components/Navigation.vue";
-import db from "./db"; //eslint-disable-line
+import db from "./db";
 
 export default {
   name: "App",
   data: function() {
     return {
-      user: null
+      user: null,
+      meetings: []
     };
   },
   methods: {
@@ -34,12 +41,35 @@ export default {
           name: payload,
           createdAt: Firebase.firestore.FieldValue.serverTimestamp()
         });
+    },
+    deleteMeeting: function(payload) {
+      db.collection("users")
+        .doc(this.user.uid)
+        .collection("meetings")
+        .doc(payload)
+        .delete();
     }
   },
   mounted: function() {
     Firebase.auth().onAuthStateChanged(user => {
       if (user) {
         this.user = user;
+
+        db.collection("users")
+          .doc(this.user.uid)
+          .collection("meetings")
+          .onSnapshot(snapshot => {
+            const snapData = [];
+            snapshot.forEach(doc => {
+              snapData.push({
+                id: doc.id,
+                name: doc.data().name
+              });
+            });
+            this.meetings = snapData.sort((a, b) =>
+              a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1
+            );
+          });
       }
     });
   },
--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-
 import {library} from "@fortawesome/fontawesome-svg-core";
-
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import {
   faTrash, faLink, faListUl,faRandom, faUndo, faStar, faEnvelope
 } from "@fortawesome/free-solid-svg-icons";
 
 library.add(faTrash, faLink, faListUl,faRandom, faUndo, faStar, faEnvelope);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon)
 
 Vue.config.productionTip = false;
 
